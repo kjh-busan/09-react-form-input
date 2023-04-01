8bb27fbf4ac3252918867a0f4d4fad387b29b6ed
kjh-busan <picsmash7@gmail.com>
2023-04-01 14:17:19 +0900

    feat: valid add WIP

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,49 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNamaIsValid] = useState(false);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "" || null) {
+      setEnteredNamaIsValid(false);
+      return;
+    }
+
+    setEnteredNamaIsValid(true);
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    setEnteredName("");
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {!enteredNameIsValid && <p>Name is not empty.</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
